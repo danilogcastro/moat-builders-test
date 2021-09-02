bbec835e36d55f00f04a2731665b7ecfd3c95a58
Danilo Castro <danilogcastro@hotmail.com>
2021-09-02 14:23:10 -0300

    added fields full name, username and role to User

---
+++ db/migrate/20210902172107_add_fields_to_user.rb
@@ -0,0 +1,7 @@
+class AddFieldsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :username, :string
+    add_column :users, :role, :string, default: 'user'
+  end
+end
