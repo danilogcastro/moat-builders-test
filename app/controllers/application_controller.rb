9d91367f1275c61e8814d0b2afdc68eb9967905b
Danilo Castro <danilogcastro@hotmail.com>
2021-09-02 14:24:28 -0300

    added new User fields to devise parameters sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :username, :role])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :username, :role])
+  end
 end
