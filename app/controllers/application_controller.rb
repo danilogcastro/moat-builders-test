f404820e9145145aafa258954b193900fdd5cf7f
Danilo Castro <danilogcastro@hotmail.com>
2021-09-02 15:00:32 -0300

    fixed problem with devise permitted parameters and added custom message for login error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :username, :role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :full_name, :username, :role])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :username, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :full_name, :username, :role])
   end
 end
