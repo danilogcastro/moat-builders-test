a3379d7566a62febbc2237f54aa510a5edd08368
Danilo Castro <danilogcastro@hotmail.com>
2021-09-03 19:57:42 -0300

    created seed with two users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding users..."
+User.create!(email: 'admin@admin.com', password: '123456', username: 'admin', full_name: 'Admin', role: 'admin')
+User.create!(email: 'user@user.com', password: '123456', username: 'user', full_name: 'user', role: 'user')
+puts "Done!"
